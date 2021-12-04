711fc600372e79828d48d7bd299f04fa630df458
nixinator <33lockdown33@protonmail.com>
2021-12-04 19:10:07 +0000

    add the zbook initial configuraion

--- flake.nix
+++ flake.nix
@@ -9,10 +9,20 @@
 
   outputs = { self, nixpkgs, nixos-unstable, herc, ... }@inputs: {
     nixosConfigurations = {
+
      z620  = nixpkgs.lib.nixosSystem {   #this is the hostname = some func
         system = "x86_64-linux";
         modules = [
           (import ./hosts/z620/configuration.nix)
+          herc.nixosModules.agent-service
+        ];
+        specialArgs = { inherit inputs; };
+      };
+
+     zbook  = nixpkgs.lib.nixosSystem {  
+        system = "x86_64-linux";
+        modules = [
+          (import ./hosts/zbook/configuration.nix)
           herc.nixosModules.agent-service
         ];
         specialArgs = { inherit inputs; };
